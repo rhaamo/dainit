a82360e4da3edf3148180f36db18761b1d5fb7d4
Quentin Perez <qperez@ocs.online.net>
2017-01-03 10:38:39 +0100

    :zap: remove string/[]byte cast

--- main.go
+++ main.go
@@ -29,7 +29,7 @@ func runquiet(cmd string, args ...string) error {
 }
 
 // Set's the hostname for the kernel.
-func SetHostname(hostname string) {
+func SetHostname(hostname []byte) {
 	proc, err := os.Create("/proc/sys/kernel/hostname")
 	if err != nil {
 		log.Println(err)
@@ -37,8 +37,8 @@ func SetHostname(hostname string) {
 	}
 	defer proc.Close()
 
-	n, err := proc.Write([]byte(hostname))
-	if n != len(hostname) || err != nil {
+	n, err := proc.Write(hostname)
+	if err != nil {
 		log.Println(err)
 		return
 	}
@@ -64,7 +64,7 @@ func main() {
 
 	// Set the hostname for getty to be happy.
 	if hostname, err := ioutil.ReadFile("/etc/hostname"); err == nil {
-		SetHostname(string(hostname))
+		SetHostname(hostname)
 	}
 
 	// There's a little (dare I say, a lot?) of black magic that seems to
